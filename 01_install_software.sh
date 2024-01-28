ae13d8b1b3645f4957ef12311e703625104dc0c6
Songqi Duan <songqi.duan@outlook.com>
2024-01-29 02:51:39 +0800

    Update 01_install_software.sh

--- 01_install_software.sh
+++ 01_install_software.sh
@@ -29,7 +29,7 @@ mamba create -p ${WORKING_DIR}/bbmap/39.01 -c bioconda bbmap=39.01 -y
 # -----------------------------------------------------------------------------
 # Quantification
 mamba create -p ${WORKING_DIR}/subread/2.0.6 -c bioconda subread=2.0.6 -y
-mamba create -p ${WORKING_DIR}/htseq/2.0.3 -c bioconda htseq=2.0.3 -y
+mamba create -p ${WORKING_DIR}/htseq/2.0.5 -c bioconda htseq=2.0.5 -y
 mamba create -p ${WORKING_DIR}/cufflinks/2.2.1 -c bioconda cufflinks=2.2.1 -y
 mamba create -p ${WORKING_DIR}/stringtie/2.2.1 -c bioconda stringtie=2.2.1 -y
 mamba create -p ${WORKING_DIR}/salmon/1.10.2 -c bioconda salmon=1.10.2 -y
@@ -37,15 +37,15 @@ mamba create -p ${WORKING_DIR}/kallisto/0.50.0 -c bioconda kallisto=0.50.0 -y
 mamba create -p ${WORKING_DIR}/rsem/1.3.3 -c bioconda rsem=1.3.3 -y
 # -----------------------------------------------------------------------------
 # Alternative splicing
-mamba create -p ${WORKING_DIR}/rmats/4.1.2 -c bioconda rmats=4.1.2 -y
+mamba create -p ${WORKING_DIR}/rmats/4.2.0 -c bioconda rmats=4.2.0 -y
 mamba create -p ${WORKING_DIR}/rmats2sashimiplot/3.0.0 -c bioconda rmats2sashimiplot=3.0.0 -y
 mamba create -p ${WORKING_DIR}/suppa/2.3 -c bioconda suppa=2.3 -y
 # -----------------------------------------------------------------------------
 # Others
-mamba create -p ${WORKING_DIR}/multiqc/1.17 -c bioconda multiqc=1.17 -y
-mamba create -p ${WORKING_DIR}/bedtools/2.31.0 -c bioconda bedtools=2.31.0 -y
-mamba create -p ${WORKING_DIR}/samtools/1.18 -c bioconda samtools=1.18 -y
-mamba create -p ${WORKING_DIR}/snakemake/7.32.4 -c bioconda snakemake=7.32.4 -y
+mamba create -p ${WORKING_DIR}/multiqc/1.19 -c bioconda multiqc=1.19 -y
+mamba create -p ${WORKING_DIR}/bedtools/2.31.1 -c bioconda bedtools=2.31.1 -y
+mamba create -p ${WORKING_DIR}/samtools/1.19.2 -c bioconda samtools=1.19.2 -y
+mamba create -p ${WORKING_DIR}/snakemake/8.3.2 -c bioconda snakemake=8.3.2 -y
 mamba create -p ${WORKING_DIR}/sambamba/1.0 -c bioconda sambamba=1.0 -y
 mamba create -p ${WORKING_DIR}/picard/3.1.0 -c bioconda picard=3.1.0 -y
 mamba create -p ${WORKING_DIR}/umi_tools/1.1.4 -c bioconda umi_tools=1.1.4 -y
@@ -54,7 +54,7 @@ mamba create -p ${WORKING_DIR}/environment-modules/5.1.1 -c bioconda environment
 # MeRIP-seq
 mamba create -p ${WORKING_DIR}/homer/4.11 -c bioconda homer=4.11 -y
 mamba create -p ${WORKING_DIR}/macs2/2.2.9.1 -c bioconda macs2=2.2.9.1 -y
-mamba create -p ${WORKING_DIR}/meme/5.5.4 -c bioconda meme=5.5.4 -y
+mamba create -p ${WORKING_DIR}/meme/5.5.5 -c bioconda meme=5.5.5 -y
 mamba create -p ${WORKING_DIR}/piranha/1.2.1 -c bioconda piranha=1.2.1 -y
 # -----------------------------------------------------------------------------
 # ChIP-seq
@@ -74,9 +74,9 @@ conda create -p ${WORKING_DIR}/blast/2.13.0 -c bioconda blast=2.13.0 -y
 conda create -p ${WORKING_DIR}/rmblast/2.11.0 -c bioconda rmblast=2.11.0 -y
 # -----------------------------------------------------------------------------
 # Structure
-conda create -p ${WORKING_DIR}/autodock-vina/1.1.2 -c bioconda autodock-vina=1.1.2 -y
-conda create -p ${WORKING_DIR}/gromacs/2021.3 -c bioconda gromacs=2021.3 -y
-conda create -p ${WORKING_DIR}/openbabel/3.1.1 -c bioconda openbabel=3.1.1 -y
+mamba create -p ${WORKING_DIR}/autodock-vina/1.1.2 -c bioconda autodock-vina=1.1.2 -y
+mamba create -p ${WORKING_DIR}/gromacs/2021.3 -c bioconda gromacs=2021.3 -y
+mamba create -p ${WORKING_DIR}/openbabel/3.1.1 -c bioconda openbabel=3.1.1 -y
 # -----------------------------------------------------------------------------
 # Single-cell RNA-seq
 mamba create -p ${WORKING_DIR}/velocyto.py/0.17.17 -c bioconda velocyto.py=0.17.17 -y
