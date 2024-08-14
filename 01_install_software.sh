bad7f3acbe186d47c786e2074684a7c7a3e73c71
Songqi Duan <songqi.duan@outlook.com>
2024-08-14 15:24:06 -0700

    Update 01_install_software.sh

--- 01_install_software.sh
+++ 01_install_software.sh
@@ -5,7 +5,7 @@ WORKING_DIR=$1
 
 # -----------------------------------------------------------------------------
 # R
-mamba create -p ${WORKING_DIR}/r/4.2.3 -c conda-forge r-base=4.2.3 r-seurat r-devtools r-tidyverse -y
+mamba create -p ${WORKING_DIR}/r/4.4.1 -c conda-forge r-base=4.4.1 r-seurat r-devtools r-tidyverse -y
 # -----------------------------------------------------------------------------
 # Download data
 # mamba create -p ${WORKING_DIR}/sratoolkit/2.5.7 -c daler sratoolkit=2.5.7 -y
@@ -14,7 +14,7 @@ mamba create -p ${WORKING_DIR}/entrez-direct/21.6 -c bioconda entrez-direct=21.6
 # -----------------------------------------------------------------------------
 # Quality control
 mamba create -p ${WORKING_DIR}/fastp/0.23.4 -c bioconda fastp=0.23.4 -y
-mamba create -p ${WORKING_DIR}/falco/1.2.1 -c bioconda falco=1.2.1 -y
+mamba create -p ${WORKING_DIR}/falco/1.2.3 -c bioconda falco=1.2.3 -y
 mamba create -p ${WORKING_DIR}/fastqc/0.12.1 -c bioconda fastqc=0.12.1 -y
 mamba create -p ${WORKING_DIR}/fastq-pair/1.0 -c bioconda fastq-pair=1.0 -y
 mamba create -p ${WORKING_DIR}/fastq-screen/0.15.3 -c bioconda fastq-screen=0.15.3 -y
@@ -23,8 +23,8 @@ mamba create -p ${WORKING_DIR}/trimmomatic/0.39 -c bioconda trimmomatic=0.39 -y
 # -----------------------------------------------------------------------------
 # Mapping
 mamba create -p ${WORKING_DIR}/hisat2/2.2.1 -c bioconda hisat2=2.2.1 -y
-mamba create -p ${WORKING_DIR}/star/2.7.11a -c bioconda star=2.7.11a -y
-mamba create -p ${WORKING_DIR}/bowtie2/2.5.2 -c bioconda bowtie2=2.5.2 -y
+mamba create -p ${WORKING_DIR}/star/2.7.11b -c bioconda star=2.7.11b -y
+mamba create -p ${WORKING_DIR}/bowtie2/2.5.4 -c bioconda bowtie2=2.5.4 -y
 mamba create -p ${WORKING_DIR}/bwa/0.7.17 -c bioconda bwa=0.7.17 -y
 mamba create -p ${WORKING_DIR}/bbmap/39.01 -c bioconda bbmap=39.01 -y
 # -----------------------------------------------------------------------------
@@ -32,23 +32,23 @@ mamba create -p ${WORKING_DIR}/bbmap/39.01 -c bioconda bbmap=39.01 -y
 mamba create -p ${WORKING_DIR}/subread/2.0.6 -c bioconda subread=2.0.6 -y
 mamba create -p ${WORKING_DIR}/htseq/2.0.5 -c bioconda htseq=2.0.5 -y
 mamba create -p ${WORKING_DIR}/cufflinks/2.2.1 -c bioconda cufflinks=2.2.1 -y
-mamba create -p ${WORKING_DIR}/stringtie/2.2.1 -c bioconda stringtie=2.2.1 -y
-mamba create -p ${WORKING_DIR}/salmon/1.10.2 -c bioconda salmon=1.10.2 -y
-mamba create -p ${WORKING_DIR}/kallisto/0.50.0 -c bioconda kallisto=0.50.0 -y
+mamba create -p ${WORKING_DIR}/stringtie/2.2.3 -c bioconda stringtie=2.2.3 -y
+mamba create -p ${WORKING_DIR}/salmon/1.10.3 -c bioconda salmon=1.10.3 -y
+mamba create -p ${WORKING_DIR}/kallisto/0.51.0 -c bioconda kallisto=0.51.0 -y
 mamba create -p ${WORKING_DIR}/rsem/1.3.3 -c bioconda rsem=1.3.3 -y
 # -----------------------------------------------------------------------------
 # Alternative splicing
-mamba create -p ${WORKING_DIR}/rmats/4.2.0 -c bioconda rmats=4.2.0 -y
+mamba create -p ${WORKING_DIR}/rmats/4.3.0 -c bioconda rmats=4.3.0 -y
 mamba create -p ${WORKING_DIR}/rmats2sashimiplot/3.0.0 -c bioconda rmats2sashimiplot=3.0.0 -y
 mamba create -p ${WORKING_DIR}/suppa/2.3 -c bioconda suppa=2.3 -y
 # -----------------------------------------------------------------------------
 # Others
-mamba create -p ${WORKING_DIR}/multiqc/1.19 -c bioconda multiqc=1.19 -y
+mamba create -p ${WORKING_DIR}/multiqc/1.23 -c bioconda multiqc=1.23 -y
 mamba create -p ${WORKING_DIR}/bedtools/2.31.1 -c bioconda bedtools=2.31.1 -y
-mamba create -p ${WORKING_DIR}/samtools/1.19.2 -c bioconda samtools=1.19.2 -y
-mamba create -p ${WORKING_DIR}/snakemake/8.3.2 -c bioconda snakemake=8.3.2 -y
-mamba create -p ${WORKING_DIR}/sambamba/1.0 -c bioconda sambamba=1.0 -y
-mamba create -p ${WORKING_DIR}/picard/3.1.0 -c bioconda picard=3.1.0 -y
+mamba create -p ${WORKING_DIR}/samtools/1.20 -c bioconda samtools=1.20 -y
+mamba create -p ${WORKING_DIR}/snakemake/8.18.0 -c bioconda snakemake=8.18.0 -y
+mamba create -p ${WORKING_DIR}/sambamba/1.0.1 -c bioconda sambamba=1.0.1 -y
+mamba create -p ${WORKING_DIR}/picard/3.2.0 -c bioconda picard=3.2.0 -y
 mamba create -p ${WORKING_DIR}/umi_tools/1.1.4 -c bioconda umi_tools=1.1.4 -y
 mamba create -p ${WORKING_DIR}/environment-modules/5.1.1 -c bioconda environment-modules=5.1.1 -y
 # -----------------------------------------------------------------------------
@@ -62,13 +62,13 @@ mamba create -p ${WORKING_DIR}/piranha/1.2.1 -c bioconda piranha=1.2.1 -y
 mamba create -p ${WORKING_DIR}/deeptools/3.5.4 -c bioconda deeptools=3.5.4 -y
 # -----------------------------------------------------------------------------
 # DNA methylation
-mamba create -p ${WORKING_DIR}/bismark/0.24.2 -c bioconda bismark=0.24.2 -y
+# mamba create -p ${WORKING_DIR}/bismark/0.24.2 -c bioconda bismark=0.24.2 -y
 # -----------------------------------------------------------------------------
 # SNP
-mamba create -p ${WORKING_DIR}/gatk4/4.4.0.0 -c bioconda gatk4=4.4.0.0 -y
+# mamba create -p ${WORKING_DIR}/gatk4/4.4.0.0 -c bioconda gatk4=4.4.0.0 -y
 # -----------------------------------------------------------------------------
 # Nanopore sequencing
-mamba create -p ${WORKING_DIR}/nanopolish/0.14.0 -c bioconda nanopolish=0.14.0 -y
+# mamba create -p ${WORKING_DIR}/nanopolish/0.14.0 -c bioconda nanopolish=0.14.0 -y
 # -----------------------------------------------------------------------------
 # Genome
 conda create -p ${WORKING_DIR}/blast/2.13.0 -c bioconda blast=2.13.0 -y
@@ -90,12 +90,12 @@ PYSCENIC=${WORKING_DIR}/pyscenic/0.12.1
 mamba create -p $PYSCENIC python=3.10 -y
 ${WORKING_DIR}/pyscenic/0.12.1/bin/pip install pyscenic==0.12.1
 # CellPhoneDB
-CELLPHONEDB=${WORKING_DIR}/cellphonedb/4.0.0
+CELLPHONEDB=${WORKING_DIR}/cellphonedb/5.0.1
 mamba create -p $CELLPHONEDB python=3.8 -y
-${WORKING_DIR}/cellphonedb/4.0.0/bin/pip install cellphonedb==4.0.0
+${WORKING_DIR}/cellphonedb/5.0.1/bin/pip install cellphonedb==5.0.1
 # CellBender
-CELLBENDER=${WORKING_DIR}/cellbender/0.2.2
+CELLBENDER=${WORKING_DIR}/cellbender/0.3.0
 mamba create -p $CELLBENDER python=3.7 pytorch torchvision pytables -c conda-forge -y
 cd $CELLBENDER
 git clone https://github.com/broadinstitute/CellBender.git
-${WORKING_DIR}/cellbender/0.2.2/bin/pip install -e CellBender
+${WORKING_DIR}/cellbender/0.3.0/bin/pip install -e CellBender
